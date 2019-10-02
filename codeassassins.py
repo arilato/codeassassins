7c1e640fb5d17953cdfbe4a53010d7c99d48a16d
Jihan Yin <jihan_yin@berkeley.edu>
2019-10-02 00:53:38 -0700

    done

---
+++ codeassassins.py
@@ -0,0 +1,212 @@
+import requests
+import re
+import numpy as np
+import json
+import utils
+import pickle
+import slack
+from datetime import date
+
+char_min = 5
+dictionary = utils.load_dictionary(char_min=char_min)
+save_file = "SAVEFILE.pkl"
+kill_command = "!kill"
+
+class User:
+	def __init__(self, name: str, id_: str):
+		self.name = name
+		self.id = id_
+		self.code = utils.load_random_word(dictionary)
+		self.killed = []
+		self.alive = True
+		self.has_killed = False
+
+	def set_target(self, target):
+		self.target = target
+
+	def get_status_string(self):
+		if self.alive == False:
+			return utils.set_status_message(self.alive, self.death_date, self.killer, self.killed)
+		else:
+			return utils.set_status_message(self.alive, None, None, self.killed)
+
+class Game:
+	def __init__(self, channel: str, weapon: str, shield: str):
+		self.players_alive = []
+		self.players_dead = []
+		self.round = 1
+		self.round_end = "October 11th, 11:59pm"
+		self.round_start = date.today().strftime("%B %d, %Y")
+		self.weapon = weapon
+		self.shield = shield
+		self.channel = channel
+		self.channel_id = utils.get_channel_id(self.channel)
+		self.admin_id = "U94MD12MV"
+
+		# initialize players
+		member_ids, member_names = utils.get_channel_members(channel)
+		for id_, name in zip(member_ids, member_names):
+			self.players_alive.append(User(name, id_))
+
+		# set targets by creating random permutation
+		perm = np.random.permutation(len(self.players_alive))
+		for i in range(len(self.players_alive)):
+			self.players_alive[perm[i]].set_target(self.players_alive[perm[(i+1) % len(self.players_alive)]])
+
+		# Message each player who their target is
+		for player in self.players_alive:
+			message = utils.set_target_message(player.target.name) + "\n"
+			message += utils.set_codeword_message(player.code)
+			utils.send_users_message([player.id], message)
+
+		# save game state
+		save_game(self)
+
+	# user is id, not name
+	def kill(self, user: str, code: str):
+		for player in self.players_alive:
+			if player.id == user:
+				if player.target.code == code: # successful kill
+					# announce to channel
+					utils.send_channel_message(self.channel_id, \
+						utils.set_channel_kill_message(player.name, player.target.name))
+					# send success message
+					message = utils.set_success_kill_message(player.target.name)
+					utils.send_users_message([user], message)
+					player.has_killed = True
+					player.killed.append(player.target.name)
+					# send death message, update killed person
+					player.target.alive = False
+					player.target.death_date = date.today().strftime("%B %d, %Y")
+					player.target.killer = player.name
+					message = "You have been killed."
+					utils.send_users_message([player.target.id], message)
+					self.players_alive.remove(player.target)
+					self.players_dead.append(player.target)
+					# update targets
+					player.target = player.target.target
+					message = utils.set_new_target_message(player.target.name)
+					utils.send_users_message([user], message)
+					save_game(self)
+					return 1
+				else:
+					message = utils.set_fail_kill_message(player.target.name)
+					utils.send_users_message([user], message)
+					return 0
+		return -1
+
+	def end_round(self, new_round_date: str):
+		self.round += 1
+		self.round_start = date.today().strftime("%B %d, %Y")
+		self.round_end = new_round_date
+
+		players_to_remove = []
+
+		for player in self.players_alive:
+			print(player.name, player.has_killed)
+			if player.has_killed == False:  # hasn't killed, kill the player
+				utils.send_users_message([player.id], utils.set_thanos_message())
+				utils.send_channel_message(self.channel_id, \
+					utils.set_channel_kill_message("Thanos", player.target.name))	
+				players_to_remove.append(player)			
+				self.players_dead.append(player)
+			player.has_killed = False
+		save_game(self)
+
+		for player in players_to_remove:
+			self.players_alive.remove(player)
+
+
+def save_game(game: Game):
+	with open(save_file, 'wb') as f:
+		pickle.dump(game, f)
+
+
+def load_game():
+	with open(save_file, 'rb') as f:
+		return pickle.load(f)
+
+
+@slack.RTMClient.run_on(event='message')
+def process_message(**payload):
+	data = payload['data']
+	web_client = payload['web_client']
+	rtm_client = payload['rtm_client']
+
+	user = data.get('user')
+	message = data.get('text')
+
+	if user is None:
+		return
+
+	# check prefixes
+	message_words = message.split()
+	if len(message_words) == 0:
+		return
+
+	if message_words[0] == "!kill" and len(message_words) > 1:
+		if len(message_words) >= 2:
+			code = message_words[1]
+			return game.kill(user, code)
+
+	elif message_words[0] == "!weapon":
+		utils.send_users_message([user], utils.set_weapon_message(game.weapon))
+
+	elif message_words[0] == "!shield":
+		utils.send_users_message([user], utils.set_shield_message(game.shield))
+
+	elif message_words[0] == "!round":
+		utils.send_users_message([user], utils.set_round_message(game.round, game.round_start, game.round_end))
+
+	elif message_words[0] == "!status":
+		for player in game.players_alive + game.players_dead:
+			if player.id == user:
+				utils.send_users_message([user], player.get_status_string())
+
+	elif message_words[0] == "!target":
+		for player in game.players_alive:
+			if player.id == user:
+				utils.send_users_message([user], utils.set_target_message(player.target.name))
+
+	elif user == game.admin_id and len(message_words) > 0:
+		print("Admin speaking")
+		if message_words[0] == "!round_end" and len(message_words) >= 2:
+			game.end_round(message[len(message_words[0]) + 1:])
+		elif message_words[0] == "!new_round_date" and len(message_words) >= 2:
+			game.round_end = message[len(message_words[0]) + 1:]
+			utils.send_channel_message(game.channel_id, "The round end date has been updated to %s!" \
+				% game.round_end)
+		elif message_words[0] == "!will_die":
+			ndie = 0
+			for player in game.players_alive:
+				if player.has_killed == False:
+					ndie += 1
+			utils.send_users_message([game.admin_id], "%d die, %d total" %  (ndie, len(game.players_alive)))
+		elif message_words[0] == "!weapon":
+			game.weapon = message[len(message_words[0]) + 1:]
+			utils.send_channel_message(game.channel_id, "The weapon has been updated to %s!" \
+				% game.round_end)
+		elif message_words[0] == "!shield":
+			game.shield = message[len(message_words[0]) + 1:]
+			utils.send_channel_message(game.channel_id, "The shield has been updated to %s!" \
+				% game.round_end)
+		save_game(game)
+
+
+	else:
+		utils.send_users_message([user], "Unrecognized command! Or maybe unformatted. I'm disappointed.")
+
+
+if __name__ == "__main__":
+	global game
+	game = Game(channel = "test-slackbots", weapon = "fork", shield = "spoon")
+	#game = load_game()
+
+	rtm_client =  slack.RTMClient(token=utils.get_oauth_token())
+	rtm_client.start()
+
+
+
+
+
+
---
+++ utils.py
@@ -0,0 +1,205 @@
+import requests
+import re
+import numpy as np
+import json
+
+oauth_file_name = "API.token"
+admin_file_name = "ADMIN.token"
+dictionary_file_name = "dictionary.txt"
+api_pre = "https://slack.com/api/"
+
+
+###########################################################################
+#									AUTH			 					  #
+###########################################################################
+
+# gets global API token by file
+def get_oauth_token(file_name: str = oauth_file_name):
+	with open(file_name, "r") as f:
+		return re.sub(r'[^a-zA-Z0-9-]+', '', f.readline())
+
+# gets secret admin pw by file
+def get_admin_token(file_name: str = admin_file_name):
+	with open(file_name, "r") as f:
+		return re.sub(r'[^a-zA-Z0-9-]+', '', f.readline())
+
+TOKEN = get_oauth_token()
+ADMIN = get_admin_token()
+
+###########################################################################
+#								DICTIONARY			 					  #
+###########################################################################
+
+# loads all words from dictionary given minimum character limit
+def load_dictionary(file_name: str = dictionary_file_name, char_min: int = 5):
+	words = []
+	with open(file_name, "r") as f:
+		for line in f:
+			word = re.sub(r'[^a-zA-Z0-9-]+', '', line)
+			if len(word) >= char_min:
+				words.append(word)
+	return words
+
+def load_random_word(dictionary: [str]):
+	return dictionary[np.random.randint(len(dictionary))]
+
+###########################################################################
+#								CH/USR ID			 					  #
+###########################################################################
+
+# given channel name, gets internal slack id value 
+def get_channel_id(name: str):
+	URL = api_pre + "conversations.list"
+	PARAMS = {
+		'token': TOKEN,
+		'limit': 1000,
+	}
+	r = requests.get(url = URL, params = PARAMS)
+	data = r.json()
+	for ch in data['channels']:
+		if ch['name'] == 'test-slackbots':
+			return ch['id']
+	return -1
+
+# given a user id, gets the actual name of user
+def get_user_name(user_id: str):
+	URL = api_pre + "users.info"
+	PARAMS = {
+		'token': TOKEN,
+		'user': user_id,
+	}
+	r = requests.get(url = URL, params = PARAMS)
+	data = r.json()
+	if data['user']['is_bot']:
+		return -1
+	return data['user']['real_name']
+
+# gets ids and names of all members in a channel
+def get_channel_members(name: str):
+	ch_id = get_channel_id(name)
+	URL = api_pre + "channels.info"
+	PARAMS = {
+		'token': TOKEN,
+		'channel': ch_id,
+	}
+	r = requests.get(url = URL, params = PARAMS)
+	data = r.json()
+	member_ids = data['channel']['members']
+
+	member_names = [get_user_name(user_id) for user_id in member_ids]
+	member_ids = [user_id for user_id, member_name in zip(member_ids, member_names) if member_name != -1]
+	member_names = [member_name for member_name in member_names if member_name != -1]
+
+	return member_ids, member_names
+
+
+###########################################################################
+#								MESSAGING			 					  #
+###########################################################################
+
+# sets up a message to notify what the current weapon is
+def set_weapon_message(weapon: str):
+	return "The current weapon is %s!" % weapon
+
+# sets up a message to notify what the current weapon is
+def set_shield_message(shield: str):
+	return "The current shield is %s!" % shield
+
+# sets up a message to notify players of their target
+def set_target_message(target_name: str):
+	return "Your current target is %s!" % target_name
+
+#sets up a message to notify players of their target
+def set_new_target_message(target_name: str):
+	return "Your new target is %s!" % target_name
+
+# sets up message to give information on current round
+def set_round_message(round_n: int, round_start: str, round_end: str):
+	return "This is currently round %d, which started on %s. \
+		It will be ending on %s." % (round_n, round_start, round_end)
+
+# sets up thanos message
+def set_thanos_message():
+	return "You have been snapped by Thanos for not contributing to his cause!\n \
+		(You did not kill anyone this round so you died). "
+
+def set_channel_kill_message(player1: str, player2: str):
+	if player1 == "Thanos":
+		return "%s has snapped %s!" % (player1, player2)
+	return "%s has assassinated %s!" % (player1, player2)
+
+#sets up message for sending status
+def set_status_message(alive: bool, death_date: str, killer: str, killed: [str]):
+	if len(killed) == 0:
+		kill_string = "You have killed no one!  "
+	else:
+		kill_string = "You have %d kills - " % len(killed)
+	for player in killed:
+		kill_string += player + ", "
+	kill_string = kill_string[:-2]
+
+	if alive:
+		return "You are currently alive! " + kill_string
+	else:
+		returns = "You are dead! You were killed by %s on %s. " % (killer, death_date)
+		return returns + kill_string
+
+# sets up message for when you kill a user successfully
+def set_success_kill_message(user: str):
+	return "You have successfully killed %s!" % user
+
+# sets up message for when you fail to kill a user
+def set_fail_kill_message(user: str):
+	return "You have failed to kill %s! Wrong codeword :(" % user
+
+# sets up a message to notify players of their code
+def set_codeword_message(code: str):
+	message_text = "Your secret codeword is *%s*. Do not give this away to " % code
+	message_text += "anyone except the person that assassinates you. Whoever has "
+	message_text += "this codeword can take you out of the game!"
+	return message_text
+
+def create_welcome_message(user_names: [str], user_ids: [str]):
+	message_text = "Hello " + ("<@%s>, " * (len(user_ids) - 1)) % tuple(user_ids[:-1])
+	message_text = message_text[:-2] + " and <@%s>!" % user_ids[-1]
+	message_text += "\n #TODO: Welcome message"
+	return message_text
+
+
+# sends a message to a group of users. Must be user ids, not names
+def send_users_message(users: [str], message: str):
+	# open the channel
+	URL = api_pre + "conversations.open"
+	user_str = ",".join(users)
+	DATA = {
+		"token": TOKEN,
+		"users": user_str,
+	}
+	data = requests.post(url = URL, data=DATA).json()
+	if not data['ok']:
+		return -1
+
+	channel_id = data['channel']['id']
+	URL = api_pre + "chat.postMessage"
+	DATA = {
+		"token": TOKEN,
+		"channel": channel_id,
+		"text": message,
+	}
+	return requests.post(url = URL, data=DATA).json()
+
+# sends message to a channel, Takes in the channel (id, not name), and message as string
+def send_channel_message(channel: str, message: str):
+	URL = api_pre + "chat.postMessage"
+	DATA = {
+		"token": TOKEN,
+		"channel": channel,
+		"text": message,
+	}
+	return requests.post(url = URL, data=DATA).json()
+
+
+
+
+
+
